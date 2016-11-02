f81cbc4062ec3a9df186f41d9a3719541ace3080
WEDSGARBAGEDAY <glynn96@gmail.com>
2016-11-02 14:16:17 -0700

    fixed navbar and added javascript

---
+++ public/js/navbar.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(".navigation ul li").hover(function() {
+	$(this).css("background-color", "#555555");
+},
+function(){
+	$(this).css("background-color", "#cccccc");
+})
+
+$("#profilelink").click(function(){
+	window.location = "profile";
+})
+
+$("#matcheslink").click(function(){
+	window.location = "matches";
+})
+
+$("#mbtilink").click(function(){
+	window.location = "mbti";
+})
+
+$("#chatlink").click(function(){
+	window.location = "chat";
+})
