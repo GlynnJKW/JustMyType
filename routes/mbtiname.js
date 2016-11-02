2ef2c802537c51cee8820e9641b010084960967c
WEDSGARBAGEDAY <glynn96@gmail.com>
2016-11-02 15:01:25 -0700

    Added json usage for mbti pages - mbtidata.json

--- routes/mbtiname.js
+++ routes/mbtiname.js
@@ -1,7 +1,6 @@
 exports.view	=	function(req,	res)	{
 		//	controller	code	goes	here
     var	name	=	req.params.name;
-    res.render('mbtiname', {
-      'typeName': name
-    });
+    var mbtidata = require('../mbtidata.json');
+    res.render('mbtiname', mbtidata[name]);
 };
