acf2af38b0c97f609cc61bad562c7002d11cebb0
WEDSGARBAGEDAY <glynn96@gmail.com>
2016-11-03 16:12:03 -0700

    added cookies and json sorting, still broken. also edit and preferences html added

---
+++ public/js/user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function login(){
+  console.log("clicked");
+  if($("#username").val() != ""){
+    setCookie("typsy_cur_user", $("#username").val(), 1);
+  }
+  window.location = "profile";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
