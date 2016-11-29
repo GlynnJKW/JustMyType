b5479bb1dafd9fa8a6fd74d3313ed3b7aef561bd
Glynn Williams <Glynn Williams>
2016-11-28 22:14:59 -0800

    fixed minor logout problem

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -119,3 +119,11 @@ $("#chatlink").click(function(){
 $("#profilelink").click(function(){
 	window.location = "/profile";
 })
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
